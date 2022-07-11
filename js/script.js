b8cec473005ae0dc36de53dc11184800c2522b10
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-11 13:36:48 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('outputText');
 
+let guessedNumber = [Math.floor(Math.random() * 100)];
+
+btn.addEventListener('click', function(){
+    let input = document.getElementById('userInput');
+    if(input === guessedNumber){
+        output.innerHTML = `Great! you guessed right, ${guessedNumber}`;
+        input.value = '';
+    } else if(input < guessedNumber){
+        output.innerHTML = `Your guess is low! Try again!`;
+        input.value = '';
+    };
+     if(input > guessedNumber){
+        output.innerHTML = `Your guess is too high! Try again!`
+    }
+    input.value = '';
+});
